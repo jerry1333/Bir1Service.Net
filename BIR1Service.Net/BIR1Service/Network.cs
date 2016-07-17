8ac502be7389fd0eb464e5d508f2493e6ccf947e
jerry1333 <poczta@jerry1333.net>
2016-07-17 23:58:31 +0200

    Network functions to send data to server

--- BIR1Service.Net/BIR1Service/Network.cs
+++ BIR1Service.Net/BIR1Service/Network.cs
@@ -8,6 +8,90 @@ namespace BIR1Service
 {
     internal static class Network
     {
+        public static string MakeRequest(string regon, string silosId, RaportFiz type, string sid = null)
+        {
+            try
+            {
+                return MakeRequest(GetMethodUri(Method.DanePobierzPelnyRaport), Raport.PrepareDataString(Method.DanePobierzPelnyRaport, regon, type.ToString(), silosId), sid);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static string MakeRequest(string regon, string silosId, RaportPraw type, string sid = null)
+        {
+            try
+            {
+                return MakeRequest(GetMethodUri(Method.DanePobierzPelnyRaport), Raport.PrepareDataString(Method.DanePobierzPelnyRaport, regon, type.ToString(), silosId), sid);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static string MakeRequest(SearchBy search, string[] data, string sid = null)
+        {
+            try
+            {
+                return MakeRequest(GetMethodUri(Method.DaneSzukaj), Raport.PrepareSearchString(search, data), sid);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static string MakeRequest(Method type, string data, string sid = null)
+        {
+            try
+            {
+                if (type == Method.DaneSzukaj) throw new ArgumentOutOfRangeException(nameof(type));
+
+                return MakeRequest(GetMethodUri(type), Raport.PrepareDataString(type, data), sid);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static string MakeRequest(string uri, string data, string sid = null)
+        {
+            try
+            {
+                if (uri.IsNullOrEmpty()) throw new ArgumentNullException(nameof(uri));
+
+                var req = (HttpWebRequest) WebRequest.Create(uri);
+                req.Method = "POST";
+                req.ContentType = "application/json";
+                req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0";
+                req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                req.Timeout = 7000;
+                req.AutomaticDecompression = DecompressionMethods.GZip;
+                req.ContentLength = data.IsNullOrEmpty() ? 0 : data.Length;
+                if (!sid.IsNullOrEmpty())
+                    req.Headers.Add("sid", sid);
+
+                var os = new StreamWriter(req.GetRequestStream());
+                os.Write(data);
+                os.Flush();
+                os.Close();
+                var resp = (HttpWebResponse) req.GetResponse();
+                var respStream = resp.GetResponseStream();
+                if (respStream == null) return null;
+
+                var sr = new StreamReader(respStream);
+                return sr.ReadToEnd();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public static string GetMethodUri(Method type)
         {
             return Config.TestServerRequests ? $"{Config.TestServerBaseUri}{type}" : $"{Config.ServerBaseUri}{type}";
