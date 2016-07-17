710b8254960746d4f36b30fc5679f81950ce0e78
jerry1333 <poczta@jerry1333.net>
2016-07-17 23:59:13 +0200

    Captcha functions

--- BIR1Service.Net/BIR1Service/Captcha.cs
+++ BIR1Service.Net/BIR1Service/Captcha.cs
@@ -9,6 +9,73 @@ namespace BIR1Service
 {
     internal static class Captcha
     {
-        
+        public static bool IsCaptchaNeeded(string sid)
+        {
+            try
+            {
+                if (sid.IsNullOrEmpty()) throw new ArgumentNullException(nameof(sid));
+
+                var response = Network.MakeRequest(Method.GetValue, ServiceStatus.KomunikatKod.ToString(), sid);
+                dynamic dane = JsonConvert.DeserializeObject(response);
+
+                return dane.d == KomunikatKod.NeedCaptcha;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static string GetCaptcha(string sid)
+        {
+            try
+            {
+                if (sid.IsNullOrEmpty()) throw new ArgumentNullException(nameof(sid));
+
+                var response = Network.MakeRequest(Method.PobierzCaptcha, null, sid);
+                dynamic dane = JsonConvert.DeserializeObject(response);
+                return dane.d.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static Image Base64ToImage(string base64String)
+        {
+            try
+            {
+                if (base64String.IsNullOrEmpty()) return null;
+
+                var imageBytes = Convert.FromBase64String(base64String);
+                using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+                {
+                    var image = Image.FromStream(ms, true);
+                    return image;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static bool ValidateCaptcha(string sid, string input)
+        {
+            try
+            {
+                if (sid.IsNullOrEmpty()) throw new ArgumentNullException(nameof(sid));
+                if (input.IsNullOrEmpty()) throw new ArgumentNullException(nameof(input));
+
+                var response = Network.MakeRequest(Method.SprawdzCaptcha, input, sid);
+                dynamic dane = JsonConvert.DeserializeObject(response);
+                return Convert.ToBoolean(dane.d.ToString());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
